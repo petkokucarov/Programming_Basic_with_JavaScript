792f36bb2497efc3cd3dd57d7a467f3f7ac8a3f9
Pecata <52631046+petkokucarov@users.noreply.github.com>
2023-10-26 15:34:47 +0300

    Add files via upload

---
+++ FIRST-STEP-IN-CODING/function dayOfWeek.js
@@ -0,0 +1,35 @@
+function dayOfWeek(input) {
+
+    if (input == "Mondey") {
+        console.log(1);
+    }
+
+    else if (input == "Tuesday") {
+        console.log(2);
+    }
+
+    else if (input == "Thirsday") {
+        console.log(3);
+    }
+
+    else if (input == "Wensday") {
+        console.log(4);
+    }
+
+    else if (input == "Fridey") {
+        console.log(5);
+    }
+
+    else if (input == "Sataday") {
+        console.log(6);
+    }
+
+    else if (input == "Sunday") {
+        console.log(7);
+    }
+
+    else
+        console.log("eror");
+}
+
+dayOfWeek();
